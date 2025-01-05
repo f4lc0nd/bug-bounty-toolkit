aadc330102ea868414fcab8e8f363711d8e3f9b8
f4lc0nd <sofalcons@outlook.com>
2025-01-05 12:14:44 +0100

    5: adding more bash scripts

---
+++ bash/js-XSS.sh
@@ -0,0 +1,3 @@
+# XSS from javascript hidden params
+
+assetfinder *.com | gau | egrep -v '(.css|.svg)' | while read url; do vars=$(curl -s $url | grep -Eo "var [a-zA-Z0-9]+" | sed -e 's,'var','"$url"?',g' -e 's/ //g' | grep -v '.js' | sed 's/.*/&=xss/g'); echo -e "\e[1;33m$url\n\e[1;32m$vars"
